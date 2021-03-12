6bce3a2c8cb6f1c41e2107948546563b5002f263
Anna <annazmei2014@gmail.com>
2021-03-12 21:30:14 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+function sorting(a, b) {
+  return a - b;
+}
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+}
+  array.sort(sorting);
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+}
+  array.sort(sorting);
+  return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
 }
+  sum = 0;
+  for (let i = 0; i != array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum/array.length;
+}
+
